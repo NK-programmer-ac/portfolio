b544426e62b1ab3d028ea18e4fcb7021fda02933
NK-programmer-ac <160286442+NK-programmer-ac@users.noreply.github.com>
2024-05-09 23:03:02 +0900

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+  // var pics_src = new Array("muscleTraning.JPG","mountFuji.JPG","elementPicture.JPG");
+  //   var num = -1;
+
+  //   slideshow_timer();
+
+  //   function slideshow_timer(){
+  //       if (num == 2){
+  //           num = 0;
+  //       } 
+  //       else {
+  //           num ++;
+  //       }
+  //       document.getElementById("slideImg").src=pics_src[num];
+  //       setTimeout("slideshow_timer()",1000); 
+  //   }
